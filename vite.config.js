e2e0b3d1123ddbdd967d42ccdd0f2c46421d3f47
Matt Anderson <matt_anderson@live.com>
2025-04-12 04:20:01 +1200

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()]
+})
+
+export default defineConfig({
+  base: '/your-repo-name/', // Important!
+  plugins: [react()],
+})
