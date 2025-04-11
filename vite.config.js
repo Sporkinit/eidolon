72462cd1c8b99e6763ad9412a80d3cc33f6ff3b0
Matt Anderson <matt_anderson@live.com>
2025-04-12 05:09:53 +1200

    Rewrite clean vite.config.js to fix export error

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
-})
-
-export default defineConfig({
-  base: '/your-repo-name/', // Important!
+  base: '/pokedex-vite/',
   plugins: [react()],
 })
