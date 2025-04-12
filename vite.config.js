acbe7893eda65796e72f434e482757a050a3c27a
Matt Anderson <matt_anderson@live.com>
2025-04-12 12:55:19 +1200

    Fix base path for GitHub Pages and local dev

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/pokedex-vite/',
+  base: process.env.NODE_ENV === 'production' ? '/pokedex-vite/' : '/',
   plugins: [react()],
 })
