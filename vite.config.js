17cac6267dfc04fe1c93485286242a45c9ed1966
Matt Anderson <matt_anderson@live.com>
2025-06-23 13:13:31 +1200

    Update: new content or style tweaks

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/pokedex-vite/',  // <-- MATCH your GitHub repo name exactly
+  base: '/eidolon/',  // <-- MATCH your GitHub repo name exactly
   plugins: [react()],
 })
